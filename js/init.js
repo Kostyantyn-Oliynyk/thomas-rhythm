e338b8234b871457300ae9ad7c0d3648e2ac23f6
Kostyantyn Oliynyk <kostyantyn91@gmail.com>
2018-03-25 20:14:31 +0200

    add portfolio

--- js/init.js
+++ js/init.js
@@ -1,4 +1,14 @@
 $( function() {
     // intit tabs
     $( "#tabs" ).tabs();
+    $( "#tabs-portfolio" ).tabs();
+
+    // init slick carousel 
+    $('.reviews-carousel').slick({      dots: true,      prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',      nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>',  });
+
+    // position
+   $('.portfolio-nav').position({
+    portfolionav: '<div class="tabs-portfolio-nav">'
+   })
 } );
+ 
