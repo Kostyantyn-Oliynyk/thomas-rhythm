6ef4f62a4e6351b35b18d06757af04612c70640c
Kostyantyn Oliynyk <kostyantyn91@gmail.com>
2018-03-29 00:09:50 +0200

    added what people say

--- js/init.js
+++ js/init.js
@@ -4,11 +4,10 @@ $( function() {
     $( "#tabs-portfolio" ).tabs();
 
     // init slick carousel 
-    $('.reviews-carousel').slick({      dots: true,      prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',      nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>',  });
-
-    // position
-   $('.portfolio-nav').position({
-    portfolionav: '<div class="tabs-portfolio-nav">'
-   })
+    $('.reviews-carousel').slick({    
+        dots: true,      
+        prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',     
+        nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>', 
+    });
 } );
  
